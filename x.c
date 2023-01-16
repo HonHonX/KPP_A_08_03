c619d1b9d9217fa9e9c78abf14e8d0f6a2a437f9
Sherry <119943082+HonHonX@users.noreply.github.com>
2023-01-16 23:56:59 +0100

    Update x.c

--- x.c
+++ x.c
@@ -13,8 +13,6 @@ int main () {
 	printf("\n *  ( 7   7)  *                        * ");
 	printf("\n *   ´´´´´´   *                ©S.Buch * ");
 	printf("\n *************************************** \n\n");
-	
-	char ausgabe[81];
 		
 	//Vornamen
 	char* vn[5];
@@ -32,14 +30,18 @@ int main () {
 	vn[3] = "Maurer";
 	vn[4] = "Buch";
 	
+	//Ausgabe Array
+	char ausgabe[81];
+	
 	for (int i=0; i<5; i++){
 		
 		strcpy(ausgabe,vn[i]);
 		int vnL = strlen(vn[i]);
 		int nnL = strlen(nn[i]);
 		int space = 79-vnL-nnL;
-		for(int j=0; j<space; j++)
-			strcat(ausgabe,"_");		
+		for(int j=0; j<space; j++) {
+			strcat(ausgabe,"_")
+		}		
 		strcat(ausgabe,nn[i]);		
 		printf("\n%s",ausgabe);	
 	}
@@ -49,3 +51,24 @@ int main () {
 	printf("\n\n");
 	return 0;
 }
+
+/**
+ 22   char ausgabe[81];
+ 23   printf("\n");
+ 24   for(int i=0; i<5; i++){
+ 25     strcpy(ausgabe, vorname[i]);
+ 26     int laenge_eins = strlen(vorname[i]);
+ 27     int laenge_zwei = strlen(nachname[i]);
+ 28     int laenge = 79 - laenge_eins - laenge_zwei;
+ 29     for(int n=0; n<laenge; n++){
+ 30       strcat(ausgabe, " ");
+ 31     }
+ 32     strcat(ausgabe, nachname[i]);
+ 33     printf("%s\n", ausgabe);
+ 34     printf("\n");
+ 35 
+ 36   }
+ 37   printf("\n\n");
+ 38 
+ 39 }
+ */
