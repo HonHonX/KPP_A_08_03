4d990be90d0946c3bc7d985ccbaf7964f492a381
Sherry <119943082+HonHonX@users.noreply.github.com>
2023-01-17 00:10:56 +0100

    Update x.c

--- x.c
+++ x.c
@@ -24,11 +24,11 @@ int main () {
 	
 	//Nachnamen
 	char* nn[5];
-	vn[0] = "Weitmann";
-	vn[1] = "Reidenbach";
-	vn[2] = "Schmidt";
-	vn[3] = "Maurer";
-	vn[4] = "Buch";
+	nn[0] = "Weitmann";
+	nn[1] = "Reidenbach";
+	nn[2] = "Schmidt";
+	nn[3] = "Maurer";
+	nn[4] = "Buch";
 	
 	//Ausgabe Array
 	char ausgabe[81];
@@ -51,46 +51,3 @@ int main () {
 	printf("\n\n");
 	return 0;
 }
-
-/**
-#include <stdio.h>
-#include <string.h>
-
-void main(){
-
-  int zahl;
-  char* vorname[5];
-   vorname[0] = "Montag";
-   vorname[1] = "Dienstag";
-   vorname[2] = "Mittwoch";
-   vorname[3] = "Donnerstag";
-   vorname[4] = "Freitag";
-
-  char* nachname[5];
-   nachname[0] = "Montag";
-   nachname[1] = "Dienstag";
-   nachname[2] = "Mittwoch";
-   nachname[3] = "Donnerstag";
-   nachname[4] = "Freitag";
-
-
-  char ausgabe[81];
-  printf("\n");
-  for(int i=0; i<5; i++){
-    strcpy(ausgabe, vorname[i]);
-    int laenge_eins = strlen(vorname[i]);
-    int laenge_zwei = strlen(nachname[i]);
-    int laenge = 79 - laenge_eins - laenge_zwei;
-    for(int n=0; n<laenge; n++){
-      strcat(ausgabe, " ");
-    }
-    strcat(ausgabe, nachname[i]);
-    printf("%s\n", ausgabe);
-    printf("\n");
-
-  }
-  printf("\n\n");
-
-}
-
- */
